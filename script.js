732521d261e9148c558e1c145f9d5d20a4dddb59
developer-tests <80037766+developermickey@users.noreply.github.com>
2022-09-20 01:52:47 +0530

    Scroll Tigger Code Added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger);
+
 gsap.from("#img1", {
     delay: 0.4,
     opacity: 0,
@@ -22,4 +24,18 @@ gsap.from("#main h1", {
     opacity: 0,
     duration: 0.7,
     y: -60
+})
+
+gsap.from("#section", {
+    ScrollTrigger: {
+        tigger: "#frist h3",
+        start: "Top 90%",
+        markers: true,
+        scrub: true,
+        ease: Expo.easeInout
+    },
+    delay: 0.4,
+    opacity: 0,
+    duration: 0.7,
+    y: 60
 })
